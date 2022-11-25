754345bfb7124b94d9f2cb9c34b738b7a4d4b50b
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-25 11:26:30 +0100

    Initial Commit

---
+++ GrigBrowser/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GrigBrowser
+//
+//  Created by Sebastien REMY on 25/11/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
